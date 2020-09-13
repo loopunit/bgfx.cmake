7948429efb6bb3173068474c1c18b62b932c01f3
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2020-09-12 17:50:25 -0700

    external renderer experiment

--- cmake/bgfx.cmake
+++ cmake/bgfx.cmake
@@ -39,92 +39,109 @@ else()
 	set_source_files_properties( ${BGFX_DIR}/src/amalgamated.mm PROPERTIES HEADER_FILE_ONLY ON )
 endif()
 
+add_library(bgfx_private_settings INTERFACE)
+add_library(bgfx_public_settings INTERFACE)
+
+# Renderer injection
+list(REMOVE_ITEM BGFX_SOURCES ${BGFX_DIR}/src/renderer_d3d11.cpp)
+list(REMOVE_ITEM BGFX_SOURCES ${BGFX_DIR}/src/renderer_d3d11.h)
+	
+add_library(bgfx_inj OBJECT ${BGFX_CPM_ROOT}/src/renderer_injected.cpp ${BGFX_CPM_ROOT}/src/renderer_injected.h)
+target_link_libraries( bgfx_inj PRIVATE bgfx_private_settings PUBLIC bgfx_public_settings )
+target_include_directories( bgfx_inj PRIVATE ${BGFX_DIR}/src )
+
 # Create the bgfx target
-add_library( bgfx ${BGFX_LIBRARY_TYPE} ${BGFX_SOURCES} )
+add_library( bgfx ${BGFX_LIBRARY_TYPE} ${BGFX_SOURCES} $<TARGET_OBJECTS:bgfx_inj>)
+
+target_link_libraries( bgfx PRIVATE bgfx_private_settings bgfx_inj PUBLIC bgfx_public_settings )
+
+target_link_libraries( bgfx PRIVATE bgfx_inj)
 
 if(BGFX_CONFIG_RENDERER_WEBGPU)
     include(cmake/3rdparty/webgpu.cmake)
-    target_compile_definitions( bgfx PRIVATE BGFX_CONFIG_RENDERER_WEBGPU=1)
+    target_compile_definitions( bgfx_private_settings INTERFACE BGFX_CONFIG_RENDERER_WEBGPU=1)
     if (EMSCRIPTEN)
-        target_link_options(bgfx PRIVATE "-s USE_WEBGPU=1")
+        target_link_options(bgfx_private_settings INTERFACE "-s USE_WEBGPU=1")
     else()
-        target_link_libraries(bgfx PRIVATE webgpu)
+        target_link_libraries(bgfx_private_settings INTERFACE webgpu)
     endif()
 endif()
 
 # Enable BGFX_CONFIG_DEBUG in Debug configuration
-target_compile_definitions( bgfx PRIVATE "$<$<CONFIG:Debug>:BGFX_CONFIG_DEBUG=1>" )
+target_compile_definitions( bgfx_private_settings INTERFACE "$<$<CONFIG:Debug>:BGFX_CONFIG_DEBUG=1>" )
 if(BGFX_CONFIG_DEBUG)
-	target_compile_definitions( bgfx PRIVATE BGFX_CONFIG_DEBUG=1)
+	target_compile_definitions( bgfx_private_settings INTERFACE BGFX_CONFIG_DEBUG=1)
 endif()
 
 if( NOT ${BGFX_OPENGL_VERSION} STREQUAL "" )
-	target_compile_definitions( bgfx PRIVATE BGFX_CONFIG_RENDERER_OPENGL_MIN_VERSION=${BGFX_OPENGL_VERSION} )
+	target_compile_definitions( bgfx_private_settings INTERFACE BGFX_CONFIG_RENDERER_OPENGL_MIN_VERSION=${BGFX_OPENGL_VERSION} )
 endif()
 
 if( NOT ${BGFX_OPENGLES_VERSION} STREQUAL "" )
-	target_compile_definitions( bgfx PRIVATE BGFX_CONFIG_RENDERER_OPENGLES_MIN_VERSION=${BGFX_OPENGLES_VERSION} )
+	target_compile_definitions( bgfx_private_settings INTERFACE BGFX_CONFIG_RENDERER_OPENGLES_MIN_VERSION=${BGFX_OPENGLES_VERSION} )
 endif()
 
 # Special Visual Studio Flags
 if( MSVC )
-	target_compile_definitions( bgfx PRIVATE "_CRT_SECURE_NO_WARNINGS" )
+	target_compile_definitions( bgfx_private_settings INTERFACE "_CRT_SECURE_NO_WARNINGS" )
 endif()
 
 if( BGFX_CONFIG_RENDERER_WHITELIST )
 	if( BGFX_CONFIG_RENDERER_DIRECT3D9 )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_DIRECT3D9=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_DIRECT3D9=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_DIRECT3D11 )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_DIRECT3D11=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_DIRECT3D11=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_DIRECT3D12 )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_DIRECT3D12=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_DIRECT3D12=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_GNM )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_GNM=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_GNM=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_METAL )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_METAL=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_METAL=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_NVN )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_NVN=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_NVN=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_OPENGL )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_OPENGL=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_OPENGL=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_OPENGLES ) 
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_OPENGLES=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_OPENGLES=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_VULKAN )  
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_VULKAN=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_VULKAN=1" )
 	endif()
 	if( BGFX_CONFIG_RENDERER_WEBGPU )
-		target_compile_definitions( bgfx PUBLIC "BGFX_CONFIG_RENDERER_WEBGPU=1" )
+		target_compile_definitions( bgfx_public_settings INTERFACE "BGFX_CONFIG_RENDERER_WEBGPU=1" )
 	endif()
 endif()
 
 # Includes
-target_include_directories( bgfx
-	PRIVATE
+target_include_directories( bgfx_private_settings
+	INTERFACE
 		${BGFX_DIR}/3rdparty
 		${BGFX_DIR}/3rdparty/dxsdk/include
-		${BGFX_DIR}/3rdparty/khronos
-	PUBLIC
+		${BGFX_DIR}/3rdparty/khronos)
+
+target_include_directories( bgfx_public_settings
+	INTERFACE
 		$<BUILD_INTERFACE:${BGFX_DIR}/include>
 		$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>)
 
 # bgfx depends on bx and bimg
-target_link_libraries( bgfx PUBLIC bx bimg )
+target_link_libraries( bgfx_public_settings INTERFACE bx bimg )
 
 # ovr support
 if( BGFX_USE_OVR )
-	target_link_libraries( bgfx PUBLIC ovr )
+	target_link_libraries( bgfx_public_settings INTERFACE ovr )
 endif()
 
 # Frameworks required on iOS and macOS
 if( IOS )
-	target_link_libraries (bgfx PUBLIC "-framework OpenGLES  -framework Metal -framework UIKit -framework CoreGraphics -framework QuartzCore")
+	target_link_libraries (bgfx_public_settings INTERFACE "-framework OpenGLES  -framework Metal -framework UIKit -framework CoreGraphics -framework QuartzCore")
 elseif( APPLE )
 	find_library( COCOA_LIBRARY Cocoa )
 	find_library( METAL_LIBRARY Metal )
@@ -132,7 +149,7 @@ elseif( APPLE )
 	mark_as_advanced( COCOA_LIBRARY )
 	mark_as_advanced( METAL_LIBRARY )
 	mark_as_advanced( QUARTZCORE_LIBRARY )
-	target_link_libraries( bgfx PUBLIC ${COCOA_LIBRARY} ${METAL_LIBRARY} ${QUARTZCORE_LIBRARY} )
+	target_link_libraries( bgfx_public_settings INTERFACE ${COCOA_LIBRARY} ${METAL_LIBRARY} ${QUARTZCORE_LIBRARY} )
 endif()
 
 if( UNIX AND NOT APPLE AND NOT EMSCRIPTEN AND NOT ANDROID )
@@ -142,7 +159,7 @@ if( UNIX AND NOT APPLE AND NOT EMSCRIPTEN AND NOT ANDROID )
 	#find_package(Threads REQUIRED)
 	#find_library(LIBRT_LIBRARIES rt)
 	#find_library(LIBDL_LIBRARIES dl)
-	target_link_libraries( bgfx PUBLIC ${X11_LIBRARIES} ${OPENGL_LIBRARIES})
+	target_link_libraries( bgfx_public_settings INTERFACE ${X11_LIBRARIES} ${OPENGL_LIBRARIES})
 endif()
 
 # Exclude mm files if not on OS X
